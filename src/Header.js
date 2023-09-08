79c595fa5ffdb04290b53706bee08b5b86bc62ee
Mylan Li <mylanli@hotmail.com>
2023-09-08 20:31:16 +1000

    figured out flex and justify

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,24 @@ import "./Header.css";
 
 function Header() {
     return (
-      <header className="bg-purple-200">
-        Hello
-      
+        <header className="bg-purple-200 flex justify-between">     
+            <div>
+                hekki
+            </div>
 
-        <nav>
-
-        </nav>
-      </header>
+            <nav>
+                <ul className="flex">
+                    <li>
+                        one
+                    </li>
+                    <li>
+                        two
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
-  }
+}
   
 export default Header;
   
