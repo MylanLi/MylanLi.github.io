08a7285bd6777e973a95b580cb7a19169f9ad989
Mylan Li <mylanli@hotmail.com>
2024-08-08 20:03:03 +1000

    removing last attempt library usages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header';
-import About from './components/About';
 
 function App() {
   return (
     <div>
-      <Header />
-      <About />
+      <p>testing</p>
     </div>
   );
 }
