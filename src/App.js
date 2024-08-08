bf49e87f76284b3ad3a306570590467aeeff2fb4
Mylan Li <mylanli@hotmail.com>
2024-08-08 21:14:00 +1000

    mui setup and initial exploring of grid and theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import Grid from "@mui/material/Unstable_Grid2";
+import { AppBar, Button, CssBaseline, Paper } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
 
 function App() {
   return (
-    <div>
-      <p>testing</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar>
+        
+      </AppBar>
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Paper> Welcome </Paper>
+          <Button> Test </Button>
+        </Grid>
+        <Grid xs={6}>
+          <Paper> About Me </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper> Previous Projects </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper> Contact </Paper>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#304674',
+    },
+    secondary: {
+      main: '#c6d3e3',
+    },
+    background: {
+      default: '#d8e1e8',
+    },
+  },
+  typography: {
+
+  },
+});
+
+export default theme;
